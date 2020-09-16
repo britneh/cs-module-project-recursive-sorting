b31d3e928e12b20675d7c0f07a8c35a3e8ed1cee
britneh <brhacket@gmail.com>
2020-09-16 10:12:25 -0700

    final changes

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    
+    if start > end:
+        return -1
+    else:
+        mid = (start + end)// 2
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            return binary_search(arr, target, start, mid -1)
+        else:
+            return  binary_search(arr, target, mid +1, end)
+
+chance = [15, 21, 47, 84, 96]
+print(binary_search(chance, 84, 0, 5))
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +23,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+# def agnostic_binary_search(arr, target):
     # Your code here
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,18 +1,47 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
+    #pre allocating our output array with the number of elements 
+    #we know it will hold at the end
     merged_arr = [0] * elements
 
-    # Your code here
+    # init pointers to the beginning of arrA and arrB
+    a = 0
+    b = 0
+    #compare the elements those pointers are pointing at
+    #whichever element is less than or equal
+    #loop so long as both pointers are in range of their respective arrays
+    for i in range(elements):
+        #push that element to the output arr
+        if a >= len(arrA):
+            #we've moved all the elements from arrA into merged_arr
+            #we still have elements from arrB that need to be moved
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
 
+
+        #increment the pointer pointing to the element we just pushed to the output array 
+#Now only one of the pointer is in range of its array, in other words we'e moved all elements 
+#from one array to the output array and we need to move al of the elems of the other 
+# array to the output array
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
-
+    if len (arr) > 1:
+       left = merge_sort(arr[0: len(arr) // 2])
+       right = merge_sort(arr[len(arr) //2 :])
+       arr = merge(left, right)
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
@@ -21,8 +50,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
 
